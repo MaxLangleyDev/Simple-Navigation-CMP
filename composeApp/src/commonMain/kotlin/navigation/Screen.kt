ebbab2c29d571230814417648f6eac71e0cbd224
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-05-21 17:24:14 -0700

    Update Screen.kt

--- composeApp/src/commonMain/kotlin/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/navigation/Screen.kt
@@ -1,7 +1,7 @@
 package navigation
 
 sealed interface Screen {
-    data object Screen1 : Screen
+    data object Home : Screen
     data object Screen2 : Screen
     data object Screen3 : Screen
 }
